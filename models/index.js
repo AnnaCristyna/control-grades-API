bc31959067e509aa133dcf6d60daa4c95b7142d7
Anna.Barros <annacrismb@gmail.com>
2020-07-07 12:12:40 -0300

    with modelGrade made

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
+import gradeModel from './modelGrade.js';
 
 const db = {};
 db.mongoose = mongoose;
 db.url = process.env.MONGODB;
+db.grades = gradeModel(mongoose);
 
 export { db };
