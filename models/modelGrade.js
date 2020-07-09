fb140abc95ef85026fcc07c562f9f1622b2f9637
Anna.Barros <annacrismb@gmail.com>
2020-07-09 17:45:07 -0300

    type inserted in model grade

--- models/modelGrade.js
+++ models/modelGrade.js
@@ -2,7 +2,7 @@ export default (mongoose) => {
   const schema = mongoose.Schema({
     name: { type: String, required: true },
     subject: { type: String, required: true },
-    type: { typer: String, required: true },
+    type: { type: String, required: true },
     value: {
       type: Number,
       required: true,
